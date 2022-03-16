6f0bd9b75b7289d0be22bd34f0fa32c153f19e0c
aksha <aksha@Akshay>
2022-03-16 10:54:54 +0530

    commit and push demo3

--- src/test/java/Demomodule/demo2.java
+++ src/test/java/Demomodule/demo2.java
@@ -14,6 +14,9 @@ public class demo2 {
 	{
 		String BROWSER = System.getProperty("browser");
 		System.out.println(BROWSER);
+		String URL = System.getProperty("url");
+		System.out.println(URL);
+		
 	}
 
 }
